01c403547e3255640465a0e4c1b38d37bfdb9344
AbbyJones2 <38004106+AbbyJones2@users.noreply.github.com>
2018-04-22 14:52:29 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+}
+});
+bot.login(process.env.BOT_TOKEN);
