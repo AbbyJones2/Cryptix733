bcb8decd765d8775eac0c98a9b43e1cb5b835c89
AbbyJones2 <38004106+AbbyJones2@users.noreply.github.com>
2018-04-22 16:44:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({ disableEveryone: true });
+const fs = require("fs");
 bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if (err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if (jsfile.length <= 0) {
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f}loaded!`);
+    bot.commands.set(props.help.name, props)
+  });
+
   
   bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
@@ -24,13 +42,13 @@ bot.on("message", async message => {
 
 
 
-});
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if (commandfile) commandfile.run(bot, message, args);
 
- 
+  if (cmd === `${prefix}welcome`) {
+    return message.channel.send(`To make the welcome message work make sure the name is welcome-leave in order for it to work!`);
 
-  
+  }
   if (cmd == `${prefix}info`) {
     let infoEmbed = new Discord.RichEmbed()
     let helpembed = new Discord.RichEmbed()
