315c2e5e01f89a257d1182f284c5e334e7629a95
AbbyJones2 <38004106+AbbyJones2@users.noreply.github.com>
2018-04-25 15:26:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
 
-client.on('ready', () => {
 bot.on("ready", async () => {
  console.log(`${bot.user.username} is online!`);
 
@@ -9,10 +9,19 @@ bot.user.setActivity("Use ?help",{type: "PLAYING"});
 });
 
 });
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-    
+bot.on("message", async message => {
+ if(message.author.bot) return;
+if(message.channel.type === "dm") return;
+
+ let prefix = botconfig.prefix;
+let messageArray = message.content.split(" ");
+ let cmd = messageArray[0];
+let args = messageArray.slice(1);
+
+
+ let commandfile = bot.commands.get(cmd.slice(prefix.length));
+ if(commandfile) commandfile.run(bot,message,args);
+
   }
 });
 client.login(process.env.BOT_TOKEN);
