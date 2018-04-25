d2ff632d34b47a3e90ba1a502d752f20cb692058
AbbyJones2 <38004106+AbbyJones2@users.noreply.github.com>
2018-04-25 15:23:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  bot.user.setActivity("Use ?help",{type: "PLAYING"});
+bot.on("ready", async () => {
+ console.log(`${bot.user.username} is online!`);
+
+bot.user.setActivity("Use ?help",{type: "PLAYING"});
 });
 
 client.on('message', msg => {
