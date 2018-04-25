5393558e57a0f961da95546accdaf940211255bf
AbbyJones2 <38004106+AbbyJones2@users.noreply.github.com>
2018-04-25 15:22:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  bot.user.setActivity("Use ?help",{type: "PLAYING"});
 });
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
+    
   }
 });
 client.login(process.env.BOT_TOKEN);
